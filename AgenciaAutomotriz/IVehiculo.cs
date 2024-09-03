bf3238cba81a88d173aabda640b963110e6b4389
Miguel Ramos <return.infox@gmail.com>
2024-09-03 19:51:44 -0400

    proyecto de Agenda Automotriz

---
+++ AgenciaAutomotriz/IVehiculo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgenciaAutomotriz
+{
+    internal interface IVehiculo
+    {
+        string Marca { get; set; }
+        string Modelo { get; set; }
+        int VelocidadMaxima { get; set; }
+
+        void MostrarDetalles();
+        void Acelerar(int incremento);
+    }
+}
---
+++ AgenciaAutomotriz/Vehiculo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgenciaAutomotriz
+{
+    abstract class Vehiculo : IVehiculo
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int VelocidadMaxima { get; set; }
+
+        public int NumeroDeRuedas { get; set; }
+
+        public void Acelerar(int incremento) { 
+           VelocidadMaxima = incremento + VelocidadMaxima; // explicito
+                                                           // VelocidadMaxima += incremento; // implicito
+            // Console.WriteLine(Modelo + "ha acelerado, Nueva velocidad Máxima: " + VelocidadMaxima + "km/h");
+            Console.WriteLine($"{Modelo} ha acelerado, Nueva velocidad Máxima: {VelocidadMaxima} km/h");
+        }
+        public void MostrarDetalles() {
+            Console.WriteLine($" Marca: {Marca}, Modelo: {Modelo}, Velocidad Máxima: {VelocidadMaxima} km/h, Número de Ruedas: {NumeroDeRuedas}");
+        }
+    }
+}
