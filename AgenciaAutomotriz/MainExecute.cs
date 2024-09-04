5533d06f33d79d87a438016be39791e73aaff80d
Miguel Ramos <return.infox@gmail.com>
2024-09-03 20:49:24 -0400

    clase ejectura

--- AgenciaAutomotriz/MainExecute.cs
+++ AgenciaAutomotriz/MainExecute.cs
@@ -6,7 +6,43 @@ using System.Threading.Tasks;
 
 namespace AgenciaAutomotriz
 {
-    internal class Main
+    class MainExecute
     {
+        static void Main(string[] args) { 
+            // Primer auto
+            Auto auto = new Auto();
+            auto.Marca = "Toyota";
+            auto.Modelo = "Corolla";
+            auto.VelocidadMaxima = 180;
+
+            // Segundo auto
+            Auto auto2 = new Auto();
+            auto2.Marca = "Ford";
+            auto2.Modelo = "Fiesta";
+            auto2.VelocidadMaxima = 200;
+
+            // Moticicleta
+            Motocicleta moto = new Motocicleta();
+            moto.Marca = "Harley Davidson";
+            moto.Modelo = "Iron 883";
+            moto.VelocidadMaxima = 120;
+            moto.TieneSidecar = false;
+
+            Motocicleta moto2 = new Motocicleta()
+            {
+                Marca = "BMW",
+                Modelo = "R 1250 GS",
+                VelocidadMaxima = 200,
+                TieneSidecar = true
+            };
+
+            // Mostrar los detalles
+            auto.MostrarDetalles();
+            auto2.MostrarDetalles();
+
+            moto.MostrarDetalles();
+            moto2.MostrarDetalles();
+
+        }
     }
 }
